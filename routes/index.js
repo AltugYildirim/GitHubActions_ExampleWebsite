5e0854ff6533d41fc342c50ae9db9cb6750de714
Altuğ Yıldırım <altugyildirim@Altug-MacBook-Air.local>
2020-04-01 17:29:06 +0300

    slack configured v25

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Microsoft OCP OpenHack etkinliğine hoş geldiniz! Umarım keyifli bir demo olur!',   
+    title: 'Microsoft AKS OpenHack etkinliğine hoş geldiniz! Umarım keyifli bir demo olur!',   
   });
 });
 
